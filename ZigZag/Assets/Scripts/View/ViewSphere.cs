7a94ff5c04b7fd31d983b49be63e982410224373
kosteng <2267672@mail.ru>
2019-12-08 13:00:18 +0300

    Слегка отрефакторил вьюху

--- ZigZag/Assets/Scripts/View/ViewSphere.cs
+++ ZigZag/Assets/Scripts/View/ViewSphere.cs
@@ -4,12 +4,13 @@ public class ViewSphere : MonoBehaviour
 {
     
     [SerializeField] private float _speed = 1f;
-    private bool _direction= false;
+    private bool _direction = false;
     private bool _start = false;
+    private int _leftMouseButton = 0;
     public void Move(float deltaTime)
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(_leftMouseButton))
         {
             _start = true;
             _direction = !_direction;
@@ -17,13 +18,12 @@ public class ViewSphere : MonoBehaviour
 
         if (_start && _direction)
         {
-            transform.Translate(Vector3.forward * deltaTime * _speed);
+            transform.Translate(Vector3.forward * deltaTime * _speed); 
         }
 
         if (_start && !_direction)
         {
-            transform.Translate(Vector3.right * deltaTime * _speed);
-        }
-
+                transform.Translate(Vector3.right * deltaTime * _speed);
+        }       
     }
 }
