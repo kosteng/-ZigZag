4ac115366a04f3305bff5b7484a5cc4489a8dab6
kosteng <2267672@mail.ru>
2019-12-12 20:41:08 +0300

    Добавил первый вариант фабрики

---
+++ ZigZag/Assets/Scripts/Helpers/Factory.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    public void Create ()
+    {
+       Instantiate(_prefab, Vector3.zero, Quaternion.identity);
+        Instantiate(_prefab, new Vector3 (1,0,1), Quaternion.identity);
+
+    }
+}
